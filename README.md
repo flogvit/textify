9790ea5eb59681cd2ac86dd46b59817c94acda18
Vegard Hanssen <Vegard.Hanssen@menneske.no>
2012-12-31 17:24:25 +0100

    Added toBinary and fromBinary

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # Textify
 
 Node.js library to textify text.
+
+[![Build Status](https://travis-ci.org/flogvit/textify.png)](https://travis-ci.org/flogvit/textify)
+
+
+### toBinary(text, wrap)
+
+This will convert your text to 8 bit binary numbers. **wrap** is a number for where
+to add a linebreak in the output. Without is (default 0) you will get all on one line.
+
+### fromBinary(text)
+
+This will take the 8 bit binary text and return it as ascii text. All linebreaks and
+spaces will be removed.
--- test/test_textify.js
+++ test/test_textify.js
@@ -8,5 +8,23 @@ describe('textify', function() {
 		it('should return 01100001 for a', function() {
 			textify.toBinary('a').should.eql('01100001');
 		});
+		it('should return a long string with linebreak for this is a test', function() {
+			textify.toBinary('this is a test', 8).should.eql("01110100011010000110100101110011001000000110100101110011\n00100000011000010010000001110100011001010111001101110100");
+		});
+		it('should return a long string without linebreak for this is a test', function() {
+			textify.toBinary('this is a test').should.eql("0111010001101000011010010111001100100000011010010111001100100000011000010010000001110100011001010111001101110100");
+		});
+		
+	});
+	describe('#fromBinary()', function() {
+		it('should return a for 01100001', function() {
+			textify.fromBinary('01100001').should.eql('a');
+		});
+		it('should return this is a test', function() {
+			textify.fromBinary('0111010001101000011010010111001100100000011010010111001100100000011000010010000001110100011001010111001101110100').should.eql('this is a test');
+		});
+		it('should return this is a test without linebreak', function() {
+			textify.fromBinary('01110100011010000110100101110011001000000110100101110011\n00100000011000010010000001110100011001010111001101110100').should.eql('this is a test');
+		});
 	});
 });
